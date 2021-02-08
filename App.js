fa10705fc72a2f055420a62a40bf464dbf0d37c7
Jakub Choła <jakub.chola92@gmail.com>
2021-02-08 10:28:19 +0100

    Create React Native App

--- App.js
+++ App.js
@@ -3,6 +3,26 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  
+  const [todos, setTodos] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false)
+
+  const removeTodoHandler = (id) => {
+    setTodos(todos.filter(item => item.key !== id))
+  }
+
+  const addTotoHandler = todoTitle => {
+    setTodos([
+      ...todos, 
+      {key: Math.random().toString(), value: todoTitle 
+      }]);
+    setIsAddMode(false)
+  }
+
+  const cancelTodoAdd = () => {
+    setIsAddMode(false);
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
